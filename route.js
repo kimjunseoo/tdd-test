99ff2d26e99385f45a238ddf94c78d5ce42c00bc
kimjunseoo <zz0905k@naver.com>
2024-10-28 20:28:54 +0900

    getProducts 통합 테스트

--- route.js
+++ route.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 const productController = require("./controller/product");
+
+router.get("/", productController.getProducts);
 router.post("/", productController.createProduct);
 
 module.exports = router;
--- test/intergration/products.int.spec.js
+++ test/intergration/products.int.spec.js
@@ -26,3 +26,11 @@ it("should return 500 on /api/products", async () => {
       "Product validation failed: description: Path `description` is required.",
   });
 });
+
+it("GET /api/product", async () => {
+  const response = await request(app).get("/api/product");
+  expect(response.statusCode).toBe(200);
+  expect(Array.isArray(response.body)).toBeTruthy();
+  expect(response.body[0].name).toBeDefined();
+  expect(response.body[0].description).toBeDefined();
+});
