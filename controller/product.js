5d7fdc4b8d16533c8c8652b45c27bf22794f634e
kimjunseoo <zz0905k@naver.com>
2024-10-28 17:41:17 +0900

    통합테스트 에러처리

--- controller/product.js
+++ controller/product.js
@@ -1,14 +1,12 @@
 const productModel = require("../models/Product");
 
 exports.hello = (req, res) => {
-  console.log("API RUNNING");
   res.send("Hello world");
 };
 
 exports.createProduct = async (req, res, next) => {
   try {
     const createdProduct = await productModel.create(req.body);
-    console.log(createdProduct);
     res.status(201).json(createdProduct);
   } catch (error) {
     next(error);
--- test/intergration/products.int.spec.js
+++ test/intergration/products.int.spec.js
@@ -14,3 +14,14 @@ it("POST /api/products", async () => {
   expect(response.body.name).toBe(newProductData.name);
   expect(response.body.description).toBe(newProductData.description);
 });
+
+it("should return 500 on /api/products", async () => {
+  const response = await request(app)
+    .post("/api/product")
+    .send({ name: "kim" });
+
+  expect(response.statusCode).toBe(500);
+
+  console.log(response.body);
+  //expect(response.body).toStrictEqual({ message: })
+});
